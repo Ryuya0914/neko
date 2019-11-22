5a6cab8ec98df538a3c8039833887f29f004ea30
k <kairuika.jp@gmail.com>
2019-11-22 13:46:36 +0900

    タイトルシーン追加

---
+++ MusicRun/Assets/Title/Script/titleS.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titleS : MonoBehaviour
+{
+    public string lesson_Tag = "lesson_Tag";
+    public string game_Tag = "game_Tag";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = new Ray();
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, Mathf.Infinity);
+
+            if (hit.collider)
+            {
+                if (hit.collider.gameObject.CompareTag(lesson_Tag))
+                {
+                   // SceneManager.LoadScene("YAMADA");
+                }
+                if (hit.collider.gameObject.CompareTag(game_Tag))
+                {
+                   // SceneManager.LoadScene("GAME");
+                }
+
+            }
+
+        }
+    }
+}
