fec0bc205371a2b4c7dc28c8edf176330f411f0c
sho0630 <nkcgta.sho.takeuchi@gmail.com>
2019-11-29 16:21:58 +0900

    カメラ移動するよ！

--- MusicRun/Assets/Stage/Script/camera_move.cs
+++ MusicRun/Assets/Stage/Script/camera_move.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class camera_move : MonoBehaviour
 {
-    int spd = 1;
+    int spd = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +14,11 @@ public class camera_move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(gameObject.transform.position.x>= 360)
+        if (gameObject.transform.position.x >= 360)
         {
             spd = 0;
         }
-        gameObject.transform.position = new Vector2(spd * Time.deltaTime, 0);
+        gameObject.transform.position += new Vector3(spd * Time.deltaTime, 0);
         
     }
 }
