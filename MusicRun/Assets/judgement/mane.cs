c55accb496e0d64edc7fc3e482816ee11993cb1d
sora48690110 <misaki48690110@gmail.com>
2019-11-29 13:52:35 +0900

    判定のやつ入れたよ

---
+++ MusicRun/Assets/judgement/mane.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[System.Serializable]
+//変数まとめたやつ
+class Summary
+{
+
+    [Header("判定後のノーツの色（透明）")] public Color Transparent;//透明の色
+    [Header("一度押した後再判定するまでの時間")] public float Recasttime;//再判定までの時間
+    [Header("ノーツの判定される距離（判定ポイント基準）")] public float Judge_Distance;//判定距離
+    [Header("判定ポイントからのパーフェクト範囲")] public float Perfect_Distance;//パーフェクトの範囲
+    [Header("判定ポイントからのグッド範囲")] public float Good_Distance;//グッドの範囲
+
+}
+[System.Serializable]
+class Item
+{
+    [Header("判定ポイント")]public GameObject Target;
+    [Header("ノーツその1")]public GameObject A1;
+    [Header("ノーツその2")]public GameObject A2;
+    [Header("ノーツその3")]public GameObject A3;
+    [Header("判定表示用テキスト")]public Text JugdeText;
+}
+public class mane : MonoBehaviour
+{
+    [SerializeField,Header("変数まとめたもの")] Summary Summary;
+    [SerializeField,Header("オブジェクトまとめたもの")] Item Item;
+    [HideInInspector] public bool Recast = true;
+    float A, B, C, Max;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Return_Judge();
+        }
+    }
+    //矢印と判定位置の距離の差のやつ
+    void Distance()
+    {
+        A = Mathf.Abs(Item.A1.transform.position.x - Item.Target.transform.position.x);
+        B = Mathf.Abs(Item.A2.transform.position.x - Item.Target.transform.position.x);
+        C = Mathf.Abs(Item.A3.transform.position.x - Item.Target.transform.position.x);
+        Max = A;
+        if (B < Max) Max = B;
+        if (C < Max) Max = C;
+    }
+    //矢印が消えるやつ
+    void Vanish()
+    {
+        if (Max <= Summary.Judge_Distance)
+        {
+            if (A == Max) Item.A1.GetComponent<SpriteRenderer>().material.color =Summary.Transparent;
+            else if (B == Max) Item.A2.GetComponent<SpriteRenderer>().material.color = Summary.Transparent;
+            else Item.A3.GetComponent<SpriteRenderer>().material.color = Summary.Transparent;
+        }
+    }
+    //判定のやつ
+    int Jugde()
+    {
+        int Ret=-1;
+        if (Max < Summary.Judge_Distance)
+        {
+            if (Max < Summary.Perfect_Distance)
+            {
+                Ret = 2;
+                Item.JugdeText.text = "Perfect";
+            }
+            else if (Max < Summary.Good_Distance)
+            {
+                Ret = 1;
+                Item.JugdeText.text = "Good";
+            }
+            else
+            {
+                Ret = 0;
+                Item.JugdeText.text = "Miss";
+            }
+        }
+        return Ret;
+    }
+    //再判定用のフラグを戻すやつ
+    void RecastReset()
+    {
+        Recast = true;
+    }
+    //クリックか何かしたら実行するやつ
+    public int Return_Judge()
+    {
+        int Ret = -1;
+        Distance();
+        Vanish();
+        if (Recast)
+        {
+            Ret=Jugde();
+            Recast = false;
+            Invoke("RecastReset", Summary.Recasttime);
+        }
+        else Item.JugdeText.text = "Miss";
+        return Ret;
+    }
+}
---
+++ MusicRun/Assets/judgement/move.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class move : MonoBehaviour
+{
+    [SerializeField,Header("判定ポイント")] GameObject Target;
+    [SerializeField,Header("判定表示用テキスト")] Text judge;
+    [SerializeField,Header("BGMのaudiosource入れるやつ")] AudioSource BGM;
+    [SerializeField,Header("maneのノーツ番号入れてね")] float interval;
+    [SerializeField,Header("maneをgetcomponentする用")]GameObject l;
+    [SerializeField, Header("判定ポイント過ぎた後の距離")] float distance;
+    [SerializeField, Header("ノーツの合計数")] int count;
+    mane mane;
+    void Start()
+    {
+        mane = l.GetComponent<mane>();//mane引継ぎ用
+    }
+
+    void Update()
+    {
+        Move();//move実行位置
+        
+    }
+    //ノーツ移動用メソッド
+    void Move()
+    {
+        //BGMの再生時間と各ノーツの持っている値を比較
+        if (BGM.time > interval + distance)
+        {
+            if (mane.Recast)
+            {
+                judge.text = "Miss";//判定テキスト変更
+                //何か呼ぶならここ
+            }
+            //ノーツ使いまわし用
+            interval += count;
+            //透明になっていろノーツを元に戻すよう
+            this.gameObject.GetComponent<SpriteRenderer>().material.color = Color.white;
+        }
+        //BGMとintervalを比較して差分で動かす用
+        transform.position = new Vector2((interval - BGM.time) * 10 + Target.transform.position.x, -4);
+    }
+}
