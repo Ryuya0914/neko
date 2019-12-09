5c9668d3eb4d42f3b76b303bcfff4489bb282c2f
tanaka0914 <tanaka.ryuya.0914@gmail.com>
2019-12-09 13:38:09 +0900

    playerにキラキラつけた

---
+++ MusicRun/Assets/Player/Script/Particle_P.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle_P : MonoBehaviour
+{
+    // スクリプト
+    Manager_P _manager;
+    // パーティクル
+    ParticleSystem effect;
+    // フラグ エフェクトが出ているときtrue
+    bool flg = false;
+
+    void Start()
+    {
+        _manager = gameObject.GetComponentInParent<Manager_P>();
+        effect = gameObject.GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        // パーティクル出てるかif文
+        if(flg) {
+            if(_manager.Flag_onGround) {    // 地面についているとき
+                effect.Stop();              // パーティクルを止める
+                flg = false;
+            }
+        } else {
+            if(!_manager.Flag_onGround) {   // 地面から離れた時
+                effect.Play();              // パーティクル生成
+                flg = true;
+            }
+        }
+    }
+}
