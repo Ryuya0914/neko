59830634f16a800a6dad96494bd7b4f71e065885
sora48690110 <misaki48690110@gmail.com>
2019-12-10 14:24:38 +0900

    田中さんに言われるがままにやりました（リズムのとこいじったよ）

--- MusicRun/Assets/judgement/mane.cs
+++ MusicRun/Assets/judgement/mane.cs
@@ -2,103 +2,218 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
+
 [System.Serializable]
 //変数まとめたやつ
 class Summary
 {
 
-    [Header("判定後のノーツの色（透明）")] public Color Transparent;//透明の色
-    [Header("一度押した後再判定するまでの時間")] public float Recasttime;//再判定までの時間
-    [Header("ノーツの判定される距離（判定ポイント基準）")] public float Judge_Distance;//判定距離
-    [Header("判定ポイントからのパーフェクト範囲")] public float Perfect_Distance;//パーフェクトの範囲
-    [Header("判定ポイントからのグッド範囲")] public float Good_Distance;//グッドの範囲
-
+    [Header("判定後のノーツの色（透明）")] public Color Transparent;
+    [Header("一度押した後再判定するまでの時間"),Range(0.0f,0.6f)] public float Recasttime;
+    [Header("ノーツの判定される距離（判定ポイント基準）"),Range(2,4)] public float Judge_Distance;
+    [Header("判定ポイントからのパーフェクト範囲"),Range(0.2f,0.4f)] public float Perfect_Distance;
+    [Header("判定ポイントからのグッド範囲"),Range(1,3)] public float Good_Distance;
+    [Header("判定ポイントからのミス範囲"),Range(0.01f,0.15f)] public float Miss_Distance;
+    [Header("判定テキストリセットまでの時間"),Range(0.2f,0.4f)] public float Reset_Time;
 }
+
+
 [System.Serializable]
 class Item
 {
-    [Header("判定ポイント")]public GameObject Target;
-    [Header("ノーツその1")]public GameObject A1;
-    [Header("ノーツその2")]public GameObject A2;
-    [Header("ノーツその3")]public GameObject A3;
-    [Header("ノーツその4")]public GameObject A4;
-    [Header("ノーツその5")]public GameObject A5;
+    [Header("ノーツ")] public Transform[] Notes;
+    [Header("フェイクノーツ")] public Transform[] Fake_Notes;
+    [Header("ノーツカラー")] public SpriteRenderer[] Notes_C;
+    [Header("フェイクノーツカラー")] public SpriteRenderer[] Fake_Notes_C;
     [Header("判定表示用テキスト")]public Text JugdeText;
+    [Header("判定ポイント")] public Transform Point;
+    [Header("パーフェクトエフェクト")] public GameObject Perfect_Efe;
+    [Header("判定音")] public AudioClip[] Sound_Effect;
 }
+
+
 public class mane : MonoBehaviour
 {
-    [SerializeField,Header("変数まとめたもの")] Summary Summary;
-    [SerializeField,Header("オブジェクトまとめたもの")] Item Item;
+    [SerializeField, Header("変数まとめたもの")] Summary Summary;
+    [SerializeField, Header("オブジェクトまとめたもの")] Item Item;
+    //再判定フラグ
     [HideInInspector] public bool Recast = true;
-    float A, B, C,D,E, Max;
+    //判定テキストリセットフラグ
+    [HideInInspector] public bool NullText = false;
+    //ノーツが透明か判断するフラグ
+    [HideInInspector] public bool Vanish_Notes = true;
+    //各ノーツの距離比較用
+    float Notes_A, Notes_B, Notes_C, Distance_Min;
+    
+
+    void Update()
+    {
+        //各ノーツの距離計算
+        Distance();
+
+        //ノーツをスルーした時の処理
+        if (Distance_Min < Summary.Miss_Distance)
+        {
+            if (Vanish_Notes)
+            {
+                Item.JugdeText.text = "Miss";
+                AudioSource audio = GetComponent<AudioSource>();
+                audio.clip = Item.Sound_Effect[2];
+                audio.Play();
+            }
+            AA();
+        }
+
+
+        //判定テキストリセットのタイミング調整
+        if (NullText) Invoke("Null_Text", Summary.Reset_Time);
+
+        //試し用
+        if (Input.GetKeyDown(KeyCode.Return)) Return_Judge();
+
+    }
+
     //矢印と判定位置の距離の差のやつ
     void Distance()
     {
-        A = Mathf.Abs(Item.A1.transform.position.x - Item.Target.transform.position.x);
-        B = Mathf.Abs(Item.A2.transform.position.x - Item.Target.transform.position.x);
-        C = Mathf.Abs(Item.A3.transform.position.x - Item.Target.transform.position.x);
-        D = Mathf.Abs(Item.A4.transform.position.x - Item.Target.transform.position.x);
-        E = Mathf.Abs(Item.A5.transform.position.x - Item.Target.transform.position.x);
-        Max = A;
-        if (B < Max) Max = B;
-        if (C < Max) Max = C;
-        if (D < Max) Max = D;
-        if (E < Max) Max = E;
+        Notes_A = Mathf.Abs(Item.Notes[0].position.x - Item.Point.position.x);
+        Notes_B = Mathf.Abs(Item.Notes[1].position.x - Item.Point.position.x);
+        Notes_C = Mathf.Abs(Item.Notes[2].position.x - Item.Point.position.x);
+        Distance_Min = Notes_A;
+        if (Notes_B < Distance_Min) Distance_Min = Notes_B;
+        if (Notes_C < Distance_Min) Distance_Min = Notes_C;
     }
+
+
     //矢印が消えるやつ
     void Vanish()
     {
-        if (Max <= Summary.Judge_Distance)
+        
+        Vanish_Notes = false;
+        if (Distance_Min <= Summary.Judge_Distance)
         {
-            if (A == Max) Item.A1.GetComponent<SpriteRenderer>().material.color =Summary.Transparent;
-            else if (B == Max) Item.A2.GetComponent<SpriteRenderer>().material.color = Summary.Transparent;
-            else if (C == Max) Item.A3.GetComponent<SpriteRenderer>().material.color = Summary.Transparent;
-            else if (D == Max) Item.A4.GetComponent<SpriteRenderer>().material.color = Summary.Transparent;
-            else Item.A5.GetComponent<SpriteRenderer>().material.color = Summary.Transparent;
+            if (Notes_A == Distance_Min)
+            {
+                Item.Notes_C[0].material.color = Summary.Transparent;
+                Item.Fake_Notes_C[0].material.color = Summary.Transparent;
+
+            }
+            else if (Notes_B == Distance_Min)
+            {
+                Item.Notes_C[1].material.color = Summary.Transparent;
+                Item.Fake_Notes_C[1].material.color = Summary.Transparent;
+
+            }
+            else
+            {
+                Item.Notes_C[2].material.color = Summary.Transparent;
+                Item.Fake_Notes_C[2].material.color = Summary.Transparent;
+
+            }
         }
     }
+    
+
+    void AA()
+    {
+        if (Distance_Min <= Summary.Judge_Distance)
+        {
+            if (Notes_A == Distance_Min)
+            {
+                if (!Vanish_Notes && Item.Notes_C[0].material.color == Color.white)
+                {
+                    Item.JugdeText.text = "miss";
+                }
+
+            }
+            else if (Notes_B == Distance_Min)
+            {
+                if (!Vanish_Notes && Item.Notes_C[1].material.color == Color.white)
+                {
+                    Item.JugdeText.text = "miss";
+                }
+
+            }
+            else
+            {
+                if (!Vanish_Notes && Item.Notes_C[2].material.color == Color.white)
+                {
+                    Item.JugdeText.text = "miss";
+                }
+            }
+        }
+
+    }
+
     //判定のやつ
     int Jugde()
     {
-        int Ret=-1;
-        if (Max < Summary.Judge_Distance)
+        AudioSource audio = GetComponent<AudioSource>();
+        int Ret = -1;
+        if (Distance_Min < Summary.Judge_Distance)
         {
-            if (Max < Summary.Perfect_Distance)
+            if (Distance_Min < Summary.Perfect_Distance)
             {
                 Ret = 2;
                 Item.JugdeText.text = "Perfect";
+                audio.clip = Item.Sound_Effect[0];
+                audio.Play();
+                Item.Perfect_Efe.SetActive(true);
             }
-            else if (Max < Summary.Good_Distance)
+            else if (Distance_Min < Summary.Good_Distance)
             {
                 Ret = 1;
                 Item.JugdeText.text = "Good";
+                audio.clip = Item.Sound_Effect[1];
+                audio.Play();
+
             }
             else
             {
                 Ret = 0;
                 Item.JugdeText.text = "Miss";
+                audio.clip = Item.Sound_Effect[2];
+                audio.Play();
             }
         }
         return Ret;
     }
+
+
     //再判定用のフラグを戻すやつ
     void RecastReset()
     {
         Recast = true;
+        Item.Perfect_Efe.SetActive(false);
+
     }
+
+    //判定テキストリセットタイミング調整用
+    public void Null_Text()
+    {
+        Item.JugdeText.text = null;
+        NullText = false;
+
+    }
+
+
     //クリックか何かしたら実行するやつ
     public int Return_Judge()
     {
         int Ret = -1;
-        Distance();
         Vanish();
         if (Recast)
         {
-            Ret=Jugde();
+            Ret = Jugde();
             Recast = false;
             Invoke("RecastReset", Summary.Recasttime);
         }
-        else Item.JugdeText.text = "Miss";
+        //else
+        //{
+        //    Item.JugdeText.text = "Miss";
+        //}
         return Ret;
     }
+
 }
--- MusicRun/Assets/judgement/move.cs
+++ MusicRun/Assets/judgement/move.cs
@@ -4,44 +4,61 @@ using UnityEngine;
 using UnityEngine.UI;
 public class move : MonoBehaviour
 {
-    [SerializeField,Header("判定ポイント")] GameObject Target;
-    [SerializeField,Header("判定表示用テキスト")] Text judge;
-    [SerializeField,Header("BGMのaudiosource入れるやつ")] AudioSource BGM;
-    [SerializeField,Header("maneのノーツ番号入れてね")] float interval;
-    [SerializeField,Header("maneをgetcomponentする用")]GameObject l,p;
-    [SerializeField, Header("判定ポイント過ぎた後の距離")] float distance;
+    [SerializeField, Header("mane引継ぎ用")] mane mane;
+    [SerializeField, Header("カラー省略用")] SpriteRenderer Sp;
+    [SerializeField, Header("BGM入れる")] AudioSource BGM;
+    [SerializeField, Header("判定ポイント")] Transform Point;
+    [SerializeField, Header("判定表示用テキスト")] Text JugdeText;
+    [SerializeField, Header("ノーツの判定切り替え")] bool switching = true;
     [SerializeField, Header("ノーツの合計数")] int count;
-    [SerializeField] float speed;
-    mane mane;
-    Life life;
-    void Start()
-    {
-        mane = l.GetComponent<mane>();//mane引継ぎ用
-        life=p.GetComponent<Life>();
-    }
-
+    [SerializeField, Header("maneのノーツ番号入れてね")] float interval;
+    [SerializeField, Header("判定ポイント過ぎた後の距離")] float distance;
     void Update()
     {
-        Move();//move実行位置
-        
+        if (switching) Move();//move実行位置
+        else Tinsel();//見掛け倒し用
     }
-    //ノーツ移動用メソッド
+
+
+
+    //判定の存在する右側ノーツ用
     void Move()
     {
+        Recycle();
+        //BGMとintervalを比較して差分で動かす用
+        transform.position = new Vector2((interval - BGM.time) * 10 + Point.position.x, Point.position.y);
+    }
+
+
+
+    //判定の存在しない見掛け倒しの左側ノーツ用
+    void Tinsel()
+    {
+        Recycle();
+        //BGMとintervalを比較して差分で動かす用
+        transform.position = new Vector2((interval - BGM.time) * -10 + Point.position.x, Point.position.y);
+    }
+
+
+    //ノーツの使いまわし用
+    void Recycle()
+    {
+        
         //BGMの再生時間と各ノーツの持っている値を比較
         if (BGM.time > interval + distance)
         {
-            if (mane.Recast)
+            if (mane.Recast && switching)
             {
-                judge.text = "Miss";//判定テキスト変更
-                life.DecreaseHP();
+                //何か呼ぶならここ
             }
             //ノーツ使いまわし用
             interval += count;
             //透明になっていろノーツを元に戻すよう
-            this.gameObject.GetComponent<SpriteRenderer>().material.color = Color.white;
+            Sp.material.color = Color.white;
+            //判定音の都合上必要
+            mane.Vanish_Notes = true;
+            mane.NullText = true;
         }
-        //BGMとintervalを比較して差分で動かす用
-        transform.position = new Vector2((interval - BGM.time) * 10 + Target.transform.position.x, -8);
+
     }
 }
